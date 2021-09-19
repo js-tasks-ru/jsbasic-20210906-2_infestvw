aaf72829e54e56de0db379a0b1cee2ab9cc0c234
Sergey Korotkov <infestvw@yandex.ru>
2021-09-19 21:31:51 +0300

    Воспользовался подсказкой для решения задачи 2-1, но остался вопрос Где функция определяет,что должна возвращать 0 если нет свойств с зарплатами?

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,12 @@
 function sumSalary(salaries) {
   // ваш код...
+  let result = 0;
+
+  for (let key in salaries) {
+    let isSalary = typeof salaries[key] === 'number' && Number.isFinite(salaries[key]);
+
+    if (isSalary) {
+      result = result + salaries[key];
+    }
+  } return result;
 }
