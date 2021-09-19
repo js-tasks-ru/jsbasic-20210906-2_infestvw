8c514f2e170891021f1dafdc686791dd7b549c9e
Sergey Korotkov <infestvw@yandex.ru>
2021-09-19 19:59:17 +0300

    Убрал лишние IF

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,10 +1,15 @@
 function checkSpam(str) {
   str = str.toUpperCase();
 
-  if (str.includes('1XBET') === true) {
-    return true;
-  } else if (str.includes('XXX') === true) {
+  if (str.includes('1XBET') || str.includes('XXX') === true) {
     return true;
   }
+  //if (str.includes('1XBET') === true) {
+  //  return true;
+  //} else if (str.includes('XXX') === true) {
+  //  return true;
+  //}
   return false;
+  
+
 }
