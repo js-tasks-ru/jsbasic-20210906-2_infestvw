d996126e66cc986cd351546584d7b1b9eb0bb14f
Sergey Korotkov <infestvw@yandex.ru>
2021-09-26 16:43:54 +0300

    Проверьте решение задания

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -13,5 +13,23 @@ let calculator = {
     return this.a * this.b;
   },
 };
+//let calculator = {
+// 
+//}
+//
+//calculator.read = function read(a, b) {
+//  this.a = a;
+//  this.b = b;
+//}
+//
+//calculator.sum = function sum() {
+// return this.a + this.b;
+//}
+//
+//calculator.mul = function mul() {
+//  return this.a * this.b;
+//}
+
+
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
